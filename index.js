82c2da8599d516f2ed1a29b2170c4d3b6d126fab
animesh chaudhri <111721851+animeshchaudhri@users.noreply.github.com>
2023-06-02 19:01:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const qrcode = require('qrcode-terminal');
+const axios = require('axios');
+const { MessageMedia } = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+ 
+
+ 
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+client.on('message', async message => {
+    const content = message.body
+	if(content  === 'pls meme') {
+		const meme = await axios("https://meme-api.com/gimme")
+    .then(res => res.data)
+    client.sendMessage(message.from, await MessageMedia.fromUrl(meme.url))
+	}
+    if(content  === 'pls cat') {
+		const cat = await axios("https://cataas.com/cat")
+    .then(res => res.data)
+    client.sendMessage(message.from, await MessageMedia.fromUrl(cat.url))
+	}
+});
+
+
+
+client.initialize();
