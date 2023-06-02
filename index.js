5eb2eb9712b7a68741756bca7fc1d9a2bcd8a3f2
animesh chaudhri <111721851+animeshchaudhri@users.noreply.github.com>
2023-06-02 20:10:08 +0530

    Updateendex.js

--- index.js
+++ index.js
@@ -1,39 +1,71 @@
 const qrcode = require('qrcode-terminal');
+const express = require('express');
 const axios = require('axios');
-
 const { MessageMedia } = require('whatsapp-web.js');
 const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
 const client = new Client({
-    authStrategy: new LocalAuth()
+  authStrategy: new LocalAuth()
+});
+
+app.get('/', (req, res) => {
+  // Generate and display the QR code
+  client.on('qr', qr => {
+    qrcode.generate(qr, { small: true }, qrCode => {
+      const html = `
+        <html>
+          <body>
+            <h1>WhatsApp QR Code</h1>
+            <pre>${qrCode}</pre>
+          </body>
+        </html>
+      `;
+  
+      res.send(html);
+    });
+  });
+
+  // Handle incoming messages
+  client.on('message', async message => {
+    const content = message.body;
+    if (content === 'Pls meme') {
+        const meme = await axios("https://meme-api.com/gimme")
+          .then(res => res.data);
     
+        client.sendMessage(message.from, await MessageMedia.fromUrl(meme.url));
+      }
+    if (content === 'pls meme') {
+      const meme = await axios("https://meme-api.com/gimme")
+        .then(res => res.data);
+  
+      client.sendMessage(message.from, await MessageMedia.fromUrl(meme.url));
+    }
+  
+    if (content === 'pls cat') {
+      const cat = await axios("https://cataas.com/cat")
+        .then(res => res.data);
+  
+      client.sendMessage(message.from, await MessageMedia.fromUrl(cat.url));
+    }
+     if (content === 'Pls cat') {
+        const cat = await axios("https://cataas.com/cat")
+          .then(res => res.data);
     
+        client.sendMessage(message.from, await MessageMedia.fromUrl(cat.url));
+      }
+      if (content === 'Animika') {
+        client.sendMessage(message.from, 'chatterbux');
+      }
+      
+  });
+  
+  // Initialize the WhatsApp client
+  client.initialize();
 });
 
- 
-
- 
-
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-client.on('message', async message => {
-    const content = message.body
-	if(content  === 'pls meme') {
-		const meme = await axios("https://meme-api.com/gimme")
-    .then(res => res.data)
-    client.sendMessage(message.from, await MessageMedia.fromUrl(meme.url))
-	}
-    if(content  === 'pls cat') {
-		const cat = await axios("https://cataas.com/cat")
-    .then(res => res.data)
-    client.sendMessage(message.from, await MessageMedia.fromUrl(cat.url))
-	}
-});
-
-
-
-client.initialize();
